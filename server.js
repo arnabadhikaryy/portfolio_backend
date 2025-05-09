d2434c87c7552e60b1bc7226fde7aca230bf3bcb
arnabadhikaryy <139059216+arnabadhikaryy@users.noreply.github.com>
2025-05-09 13:08:20 +0530

    fix cross problem

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ import cookieParser from 'cookie-parser'
 
 const app = express()
 
-app.use(cors({origin:'http://localhost:5173', credentials: true}))
+app.use(cors({origin:'https://arnabprotfolio.netlify.app', credentials: true}))
 //help to pass request.bady
 app.use(express.json());
 app.use(cookieParser());
